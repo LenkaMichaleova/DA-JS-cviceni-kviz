ad8e547b010603da8020e1009a7f7a322f33cd7c
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-03-26 19:36:58 +0100

    vyřešení úkolu podle zadání na kodim.cz

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const question = document.querySelector(".question")
+const answerText = document.querySelector(".answer__text")
+const answer = document.querySelector(".answer")
 
+question.textContent = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon."
+
+const odpoved = prompt("Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.")
+
+answerText.textContent = odpoved
+
+
+if (odpoved === "Bell") {
+    answer.classList.add("answer--correct")
+} else {
+    answer.classList.add("answer--wrong")
+}
